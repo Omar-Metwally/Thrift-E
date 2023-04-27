260a6a2064f7b7d384cb02471ac620bdf7dbc2bf
omar met <xomar.metwallyx@gmail.com>
2023-04-27 23:04:17 +0200

    v.03

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Linq;
 using static NuGet.Packaging.PackagingConstants;
 
@@ -28,19 +29,24 @@ namespace Thrift_E.Controllers
 
             List<Cart> carts = _context.Carts.Where(x => x.CustomerId == person.CustomerId).ToList();
             int LastId = _context.Orders.OrderByDescending(x => x.OrderId).FirstOrDefault().OrderId;
+            Order order = new Order();
+            order.OrderId = LastId + 1;
+            order.CustomerId = (int)person.CustomerId;
+            _context.Orders.Add(order);
+
+
             foreach (Cart Item in carts)
             {
                 Product product = _context.Products.FirstOrDefault(x => x.ProductId == Item.ProductId);
                 if (product.InstockQty >= Item.Qty)
                 {
-                    Order order = new Order();
-                    order.OrderId = LastId + 1;
-                    order.ProductId = Item.ProductId;
-                    order.CustomerId = Item.CustomerId;
-                    order.Qty = (int)Item.Qty;
-                    order.Total = (product.Price * order.Qty);
-                    product.InstockQty -= order.Qty;
-                    _context.Orders.Add(order);
+                    OrderdProduct OrderdProduct = new OrderdProduct();
+                    OrderdProduct.OrderId = order.OrderId;
+                    OrderdProduct.ProductId = Item.ProductId;
+                    OrderdProduct.Qty = (int)Item.Qty;
+                    OrderdProduct.Total = (product.Price * OrderdProduct.Qty);
+                    product.InstockQty -= OrderdProduct.Qty;
+                    _context.OrderdProducts.Add(OrderdProduct);
                     _context.Carts.Remove(Item);
                 }
                 else 
@@ -57,9 +63,21 @@ namespace Thrift_E.Controllers
             string myCookieValue = HttpContext.Request.Cookies["MyCookie"];
             var person = _context.Customers.FirstOrDefault(x => x.Cookie == myCookieValue);
 
-            var products = _context.Orders.Where(z => z.CustomerId == person.CustomerId).Include(c => c.Product).ThenInclude(p => p.Category).Include(x => x.Product.MeasureOfScale).Select(c => new OrderViewModel
+            List<Order> orders = _context.Orders.Where(x => x.CustomerId == person.CustomerId).ToList();
+
+            return View(orders);
+
+
+        }
+
+        public IActionResult Details(int OrderId)
+        {
+            string myCookieValue = HttpContext.Request.Cookies["MyCookie"];
+            var person = _context.Customers.FirstOrDefault(x => x.Cookie == myCookieValue);
+
+            var products = _context.OrderdProducts.Where(z => z.OrderId == OrderId).Include(c => c.Product).ThenInclude(l => l.Category).Include(p => p.Product.MeasureOfScale).Include(p => p.Order).Select(c => new OrderViewModel
             {
-                CustomerId = c.CustomerId,
+                CustomerId = c.Order.CustomerId,
                 ProductId = c.ProductId,
                 ProductName = c.Product.ProductName,
                 Qty = c.Qty,
@@ -67,14 +85,11 @@ namespace Thrift_E.Controllers
                 Image1 = c.Product.Image1,
                 NewOrUsed = c.Product.NewOrUsed,
                 CategoryName = c.Product.Category.CategoryName,
-                MeasureOfScaleName = c.Product.MeasureOfScale.MeasureOfScale
+                MeasureOfScaleName = c.Product.MeasureOfScale.MeasureOfScale,
+                Description = c.Product.Description,
+
             }).ToList();
             return View(products);
-        }
-
-        public IActionResult Details()
-        {
-            return View();
         }
     }
 }
